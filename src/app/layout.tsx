906a92994eff019cb7eb2b9b531ca225c9d073a3
Cumstein <kamyabaghaz@gmail.com>
2025-08-04 21:50:46 +0330

    seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,11 @@ import "./globals.css";
 import Header from "@/components/layout/header";
 import Footer from "@/components/layout/footer";
 import { SessionProviderWrapper } from "@/components/providers/SessionProviderWrapper";
-import {Toaster} from "sonner"
+import { Toaster } from "sonner";
 
 export const metadata = {
   title: "موسسه زبان های خارجی قانع",
   description: "سایت رسمی موسسه زبان های خارجی قانع",
-  meta: [
-    { name: "google-site-verification", content: "W1y_vByg7henNnWwnwccPsomFe2BiJ-niADi9k343O4" },
-    { name: "description", content: "آموزش زبان انگلیسی در شهریار با اساتید حرفه‌ای در زبان های خارجی قانع. ثبت‌ نام ترم جدید آغاز شد!" },
-    { name: "robots", content: "index, follow" }
-  ]
 };
 
 export default function RootLayout({
@@ -21,6 +16,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fa" dir="rtl">
+      <head>
+        <meta
+          name="google-site-verification"
+          content="W1y_vByg7henNnWwnwccPsomFe2BiJ-niADi9k343O4"
+        />
+        <meta
+          name="description"
+          content="آموزش زبان انگلیسی در شهریار با اساتید حرفه‌ای در زبان های خارجی قانع. ثبت‌ نام ترم جدید آغاز شد!"
+        />
+        <meta name="robots" content="index, follow" />
+      </head>
       <body className={"bg-muted min-h-screen flex flex-col"}>
         <SessionProviderWrapper>
           <Header />
